d55bd1e533b3547e8891cf70369b9df23cf2f87c
riti265 <prathambhagat892@gmail.com>
2026-04-07 17:57:58 +0530

    Add files via upload

---
+++ hospital-data.js
@@ -0,0 +1,133 @@
+const hospitalsDB = {
+
+"apollo-delhi":{
+name:"Indraprastha Apollo Hospital",
+city:"Delhi",
+beds:"700+",
+icu:"250+",
+specialties:"52+",
+desc:"Leading hospital known for cardiac and transplant care.",
+img:"image/hos.jpeg",
+address:"Sarita Vihar, New Delhi",
+type: "multi-speciality",
+map:"https://maps.google.com/maps?q=Indraprastha+Apollo+Hospital+New+Delhi&output=embed"
+},
+
+"fortis-delhi":{
+name:"Fortis Escorts Heart Institute",
+city:"Delhi",
+beds:"310+",
+icu:"100+",
+specialties:"30+",
+desc:"Top cardiac hospital in India.",
+img:"image/hos.jpeg",
+address:"Okhla Road, New Delhi",
+type: "super-speciality",
+map:"https://maps.google.com/maps?q=Fortis+Escorts+Heart+Institute+Delhi&output=embed"
+},
+
+"medanta-gurgaon":{
+name:"Medanta The Medicity",
+city:"Delhi",
+beds:"1250+",
+icu:"350+",
+specialties:"50+",
+desc:"One of India's largest hospitals.",
+img:"image/hos.jpeg",
+address:"Sector 38 Gurgaon",
+type: "super-speciality",
+map:"https://maps.google.com/maps?q=Medanta+Hospital+Gurgaon&output=embed"
+},
+
+"max-delhi":{
+name:"Max Super Speciality Hospital",
+city:"Delhi",
+beds:"500+",
+icu:"120+",
+specialties:"35+",
+desc:"Advanced cancer and cardiac care hospital.",
+img:"image/hos.jpeg",
+address:"Saket New Delhi",
+type: "multi-speciality",
+map:"https://maps.google.com/maps?q=Max+Hospital+Delhi&output=embed"
+},
+
+"aiims-delhi":{
+name:"AIIMS Hospital",
+city:"Delhi",
+beds:"2500+",
+icu:"500+",
+specialties:"60+",
+desc:"India's leading government medical institute.",
+img:"image/hos.jpeg",
+address:"Ansari Nagar New Delhi",
+type: "multi-speciality",
+map:"https://maps.google.com/maps?q=AIIMS+Delhi&output=embed"
+},
+
+"kokilaben-mumbai":{
+name:"Kokilaben Hospital",
+city:"Mumbai",
+beds:"750+",
+icu:"180+",
+specialties:"55+",
+desc:"One of Mumbai's most advanced hospitals.",
+img:"image/hos.jpeg",
+address:"Andheri West Mumbai",
+type: "multi-speciality",
+map:"https://maps.google.com/maps?q=Kokilaben+Dhirubhai+Ambani+Hospital+Mumbai&output=embed"
+},
+
+"manipal-bangalore":{
+name:"Manipal Hospital",
+city:"Bangalore",
+beds:"600+",
+icu:"150+",
+specialties:"45+",
+desc:"Trusted hospital with modern facilities.",
+img:"image/hos.jpeg",
+address:"Old Airport Road Bangalore",
+type: "multi-speciality",
+map:"https://maps.google.com/maps?q=Manipal+Hospital+Bangalore&output=embed"
+},
+
+"narayana-bangalore":{
+name:"Narayana Health City",
+city:"Bangalore",
+beds:"1000+",
+icu:"300+",
+specialties:"40+",
+desc:"Famous for cardiac surgeries.",
+img:"image/hos.jpeg",
+address:"Bommasandra Bangalore",
+type: "super-speciality",
+map:"https://maps.google.com/maps?q=Narayana+Health+City+Bangalore&output=embed"
+},
+
+"miot-chennai":{
+name:"MIOT International Hospital",
+city:"Chennai",
+beds:"1000+",
+icu:"250+",
+specialties:"63+",
+desc:"Popular hospital for orthopedic surgeries.",
+img:"image/hos.jpeg",
+address:"Manapakkam Chennai",
+type: "multi-speciality",
+map:"https://maps.google.com/maps?q=MIOT+International+Hospital+Chennai&output=embed"
+},
+
+"apollo-chennai":{
+name:"Apollo Hospitals Chennai",
+city:"Chennai",
+beds:"560+",
+icu:"200+",
+specialties:"50+",
+desc:"Leading hospital for cancer treatment.",
+img:"image/hos.jpeg",
+address:"Greams Road Chennai",
+type: "super-speciality",
+map:"https://maps.google.com/maps?q=Apollo+Hospitals+Chennai&output=embed"
+}
+
+};
