6611e44b25c5b18d30ce4b8ea206fe57ba17dcc9
riti265 <prathambhagat892@gmail.com>
2026-04-08 15:10:48 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -82,13 +82,18 @@ const upload = multer({ storage: storage });
 // EMAIL TRANSPORTER
 ////////////////////////////////////////////////////
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // false for port 587, true for port 465
+    requireTLS: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
+    },
+    tls: {
+        rejectUnauthorized: false // Helps prevent cloud server SSL blocks
     }
 });
-
 ////////////////////////////////////////////////////
 // ROUTES
 ////////////////////////////////////////////////////
